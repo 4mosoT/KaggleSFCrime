8498e5c7ca1f4e4310950b82b493b71adbf709e1
Marco <marco.a.formoso@gmail.com>
2017-01-18 12:21:06 +0100

    Added C50 data

--- models/train_svm.R
+++
@@ -1,7 +0,0 @@
-library(caret)
-load("../crime_training.Rdata")
-ctrl <- trainControl(method = "repeatedcv",number=5, repeats=3,classProbs=TRUE, summaryFunction=mnLogLoss)
-formula <- Category ~ .
-model_svm <- train (formula, tuneLength=10, data=training, method='svmLinear3',trControl=ctrl, metric="logLoss", verbose = TRUE)
-model_svm
-save(model_svm, file="model_svm.Rdata")
